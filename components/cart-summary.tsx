9c968507bd2d9f20c7bc2d3c71be744c1b3fc422
Becky Hirsch <beckyhirsch@Beckys-MacBook-Air.local>
2024-03-11 08:14:47 -0700

    attempting to fix checkout logic

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -19,7 +19,7 @@ export function CartSummary() {
       body: JSON.stringify(cartDetails || {})
     })
     console.log("I'm here");
-    console.log("I made it to here", await response.json());
+    console.log("I made it to here", response.json());
     
     // const data = await response.json()
     // console.log(`no error - ${data}`)
@@ -37,7 +37,7 @@ export function CartSummary() {
       className="mt-16 rounded-lg border-2 border-gray-200 bg-gray-50 px-4 py-6 shadow-md dark:border-gray-900 dark:bg-black sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
       <h2 id="summary-heading" className="text-lg font-medium">
-        Order summary - still testing
+        Order summary - working on it
       </h2>
 
       <dl className="mt-6 space-y-4">

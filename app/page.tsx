b1d71bcfaabd185dcf83470937dda92d7eee12ff
Becky Hirsch <beckyhirsch@Beckys-MacBook-Air.local>
2024-03-18 07:23:12 -0700

    removing message form

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function Page() {
         <ParallaxImageLeft />
         <ParallaxImageBottomRight />
       </div>
-      <Contact />
+      {/* <Contact /> */}
     </>
   )
 }

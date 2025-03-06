0e8dceda94f40741e2da53325ce171fc3eb8cc05
Becky Springer <beckyspringer@Mac.home>
2025-03-05 18:12:19 -0800

    vendor contract page updates

--- app/vendor-info/vendor-contract/page.tsx
+++ app/vendor-info/vendor-contract/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
       <Link href="/vendor-info" className="mb-[2rem] max-w-[1000px] mx-auto">
         &larr; Back to Vendor Info
       </Link>
-      <h1 className="font-Forum text-[2.5rem] md:text-6xl pb-[2rem] text-center">
+      <h1 className="font-Forum text-[1.5rem] md:text-6xl md:pb-[2rem] text-center">
         Vendor Contract
       </h1>
       <iframe className={styles.contractIframe} src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhCY0OHV3OirGTYJZepumUtcsPa7_JIaGFwe4Rhb3NPssuEdCAoDPw37WGQ-4l6Tjg0*&hosted=false" width="100%" height="100%" style={{border: 0, overflow: "hidden", minHeight: "80vh", minWidth: "100%"}}></iframe>
